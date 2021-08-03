ef282930fab9b23e199707fdcdb19e7eb88367be
kimsanglok <evertree6031@gmail.com>
2021-08-04 03:53:02 +0900

    Feature : 한글과 영어 문자열의 바이트를 계산하는 함수와 원하는 바이트까지 slice해주는 함수 구성 resolved :#9

--- airbnb_clone/src/pages/local/index.jsx
+++ airbnb_clone/src/pages/local/index.jsx
@@ -1,36 +1,152 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Header from '../../components/common/Header';
-import { LocalBox, LocalContentHeader } from './styled';
+import { LocalBox, LocalContent, LocalContentHeader, LocalContentList } from './styled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import StarRateIcon from '@material-ui/icons/StarRate';
 
 const LocalSearchPage = ({ location }) => {
 
     const [area, setArea] = useState(null);
 
     useEffect(() => {
-        setArea(searchParam('location'));
-    }, []);
+        function searchParam(key) {
+            return new URLSearchParams(location.search).get(key);
+        };
 
-    function searchParam(key) {
-        return new URLSearchParams(location.search).get(key);
+        setArea(searchParam('location'));
+    }, [location.search]);
+
+
+    //한글, 영어 문자열의 바이트 계산하는 함수
+    let getTextLength = function (str) {
+        let len = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (escape(str.charAt(i)).length === 6) {
+                len++;
+            }
+            len++;
+        }
+        return len;
     };
+
+    //한글 영어 문자열의 바이트가 45까지 slice한 문자열을 반환하는 함수
+    let getText = function (str) {
+        let len = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (escape(str.charAt(i)).length === 6) {
+                len++;
+            }
+            len++;
+            if (len > 45) {
+                return str.slice(0, i);
+            }
+        }
+    };
+
+
+
+
+    const areas = [
+        {
+            imgURL: "/images/local/area/area1.png",
+            sector: "강남의 공동 주택 전체",
+            moto: "S2 Clean & cozy room right next to Gangnam Station",
+            info: ["최대 인원 3명·침실 1개·침대 1개·욕실 1개", "에어컨·주방·무선 인터넷·세탁기"],
+            star: "4.74",
+            comments: "54",
+            heart: false,
+        },
+        {
+            imgURL: "/images/local/area/area2.png",
+            sector: "홍대의 공동 주택 전체",
+            moto: "Your beatiful long term nest,cosy& memorable",
+            info: ["최대 인원 3명·침실 1개·침대 1개·욕실 1개", "에어컨·주방·무선 인터넷·세탁기"],
+            star: "4.82",
+            comments: "83",
+            heart: true,
+        },
+        {
+            imgURL: "/images/local/area/area3.png",
+            sector: "홍대의 주거용 공간 전체",
+            moto: "[OPEN]#루프탑캠핑#홍대입구역5분#Rooftop camping#Hongdae Station",
+            info: ["최대 인원 3명·침실 1개·침대 1개·욕실 1개", "에어컨·주방·무선 인터넷·세탁기"],
+            star: "4.87",
+            comments: "47",
+            heart: false,
+        },
+        {
+            imgURL: "/images/local/area/area4.png",
+            sector: "홍대의 공동 주택 전체",
+            moto: "홍대의 당신의 아름다운 장기 렌탈집!(+ wifi에그)",
+            info: ["최대 인원 3명·침실 1개·침대 1개·욕실 1개", "에어컨·주방·무선 인터넷·세탁기"],
+            star: "4.87",
+            comments: "40",
+            heart: false,
+        },
+        {
+            imgURL: "/images/local/area/area5.png",
+            sector: "홍대의 공동 주택 전체",
+            moto: "H2] 2020년 리뉴얼!합정역1분거리 초고층 스카이뷰 최고급 레지던스.무료주차",
+            info: ["최대 인원 2명·침실 1개·침대 1개·욕실 1개", "에어컨·주방·무선 인터넷·세탁기"],
+            star: "4.94",
+            comments: "369",
+            heart: false,
+        },
+    ];
 
     return (
         <LocalBox>
             <Header />
-            <LocalContentHeader>
-                <div className="headerSub">300개 이상의 숙소</div>
-                <div className="headerTit">{area}의 숙소</div>
-                <div className="controller">
-                    <div className="btn">취소 수수료 없음</div>
-                    <div className="btn">숙소 유형</div>
-                    <div className="btn">요금</div>
-                    <div className="btn">즉시 예약</div>
-                    <div className="btn">필터 추가하기</div>
-                </div>
-                <div className="headerNotice">여행 날짜와 게스트 인원수를 입력하면 1박당 총 요금을 확인할 수 있습니다.</div>
-                <div className="coronaNotice">예약하기 전에 코로나19 관련 여행 제한 사항을 확인하세요. <span>자세히 알아보기</span></div>
-            </LocalContentHeader>
+            <LocalContent>
+
+                <LocalContentHeader>
+                    <div className="headerSub">300개 이상의 숙소</div>
+                    <div className="headerTit">{area}의 숙소</div>
+                    <div className="controller">
+                        <div className="btn">취소 수수료 없음</div>
+                        <div className="btn">숙소 유형</div>
+                        <div className="btn">요금</div>
+                        <div className="btn">즉시 예약</div>
+                        <div className="btn">필터 추가하기</div>
+                    </div>
+                    <div className="headerNotice">여행 날짜와 게스트 인원수를 입력하면 1박당 총 요금을 확인할 수 있습니다.</div>
+                    <div className="coronaNotice">예약하기 전에 코로나19 관련 여행 제한 사항을 확인하세요. <span>자세히 알아보기</span></div>
+                </LocalContentHeader>
+                <LocalContentList>
+                    {
+                        areas.map(area => (
+                            <div className="localItemBox">
+                                <div className="localItem">
+                                    <div className="localItem_img">
+                                        <img src={area.imgURL} alt="" />
+                                    </div>
+                                    <div className="localItem_content">
+                                        <div className="localItem_head">
+                                            <div className="content_sector">{area.sector}</div>
+                                            <div className="content_moto">{getTextLength(area.moto) > 45 ? getText(area.moto) + "..." : area.moto}</div>
+                                            <div className="line"></div>
+                                            <div className="content_info">{area.info[0]}</div>
+                                            <div className="content_info">{area.info[1]}</div>
+                                        </div>
+                                        <div className="localItem_foot">
+                                            <StarRateIcon />
+                                            <div className="star">{area.star}</div>
+                                            <div className="comment">(후기 {area.comments}개)</div>
+                                        </div>
+                                        <div className="heart">
+                                            {
+                                                area.heart ? (<FavoriteIcon className="heartOn" />) : (<FavoriteBorderIcon />)
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </LocalContentList>
+            </LocalContent>
         </LocalBox >
     );
 };
--- airbnb_clone/src/pages/local/styled.js
+++ airbnb_clone/src/pages/local/styled.js
@@ -4,11 +4,14 @@ export const LocalBox = styled.div`
 
 `;
 
+
+export const LocalContent = styled.div`
+    width:55%;
+    padding:50px 24px 0;
+`;
+
 export const LocalContentHeader = styled.div`
     margin-top:80px;
-    width:55%;
-    /* border:1px solid gray; */
-    padding:50px 24px 0;
     font-size:14px;
     
     .headerSub{
@@ -49,9 +52,10 @@ export const LocalContentHeader = styled.div`
     }
 
     .coronaNotice{
-        height:42px;
+        height:69px;
         display:flex;
-        align-items: flex-end;
+        align-items: center;
+        border-bottom:1px solid #dddddd;
         
         span{
             text-decoration:underline;
@@ -59,7 +63,90 @@ export const LocalContentHeader = styled.div`
             cursor:pointer;
         }
     }
-
-
 `;
 
+
+
+export const LocalContentList = styled.div`
+    .localItemBox{
+        height:250px;
+        display:flex;
+        align-items: center;
+        border-bottom:1px solid #dddddd;
+        cursor:pointer;
+        position:relative;
+
+        .localItem{
+            height:200px;
+            display:flex;
+
+            .localItem_img{
+                margin-right:10px;
+                img{
+                    width:300px;
+                    height:200px;
+                    border-radius:10px;
+                    cursor:pointer;
+                }
+            }
+
+            .localItem_content{
+                display:flex;
+                flex-direction:column;
+                justify-content: space-between;
+
+                .localItem_head{
+                    .content_sector{
+                        color:#717171;
+                        margin-bottom:8px;
+                    }
+                    .content_moto{
+                        font-size:18px;
+                        margin-bottom:10px;
+                    }
+                    .line{
+                        width:50px;
+                        border:1px solid #dddddd;
+                        margin-bottom:10px;
+                    }
+                    .content_info{
+                        margin-bottom:5px;
+                        color:#717171;
+                    }
+                }
+                .localItem_foot{
+                    display:flex;
+                    align-items: center;
+
+                    svg{
+                        color:red;
+                    }
+                    div{
+                        padding-top:4px;
+                    }
+                    .star{
+                        font-weight:700;
+                    }
+                    .comment{
+                        color:#717171;
+                        padding-left:3px;
+                    }
+                }
+
+                .heart{
+                    position:absolute;
+                    top:15px;
+                    right:0;
+                    svg{
+                        width:30px;
+                        height:30px;
+                    }
+                    .heartOn{
+                        color:red;
+                    }
+                }
+            }
+            
+        }
+    }
+`;
