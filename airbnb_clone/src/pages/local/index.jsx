b8cbbf0cbf462eae35ec228adfbc387818630430
kimsanglok <evertree6031@gmail.com>
2021-08-04 02:49:30 +0900

    Feature : 국내 상품 리스트 페이지 > Header 컴포넌트로 분리 resolved :#9

--- airbnb_clone/src/pages/local/index.jsx
+++ airbnb_clone/src/pages/local/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Header from '../../components/common/Header';
-import { LocalBox } from './styled';
+import { LocalBox, LocalContentHeader } from './styled';
 
 const LocalSearchPage = ({ location }) => {
 
@@ -18,8 +18,20 @@ const LocalSearchPage = ({ location }) => {
     return (
         <LocalBox>
             <Header />
-
-        </LocalBox>
+            <LocalContentHeader>
+                <div className="headerSub">300개 이상의 숙소</div>
+                <div className="headerTit">{area}의 숙소</div>
+                <div className="controller">
+                    <div className="btn">취소 수수료 없음</div>
+                    <div className="btn">숙소 유형</div>
+                    <div className="btn">요금</div>
+                    <div className="btn">즉시 예약</div>
+                    <div className="btn">필터 추가하기</div>
+                </div>
+                <div className="headerNotice">여행 날짜와 게스트 인원수를 입력하면 1박당 총 요금을 확인할 수 있습니다.</div>
+                <div className="coronaNotice">예약하기 전에 코로나19 관련 여행 제한 사항을 확인하세요. <span>자세히 알아보기</span></div>
+            </LocalContentHeader>
+        </LocalBox >
     );
 };
 
--- airbnb_clone/src/pages/local/styled.js
+++ airbnb_clone/src/pages/local/styled.js
@@ -4,3 +4,62 @@ export const LocalBox = styled.div`
 
 `;
 
+export const LocalContentHeader = styled.div`
+    margin-top:80px;
+    width:55%;
+    /* border:1px solid gray; */
+    padding:50px 24px 0;
+    font-size:14px;
+    
+    .headerSub{
+        padding-bottom:8px;
+    }
+
+    .headerTit{
+        font-size:32px;
+        font-weight:700;
+        letter-spacing:2px;
+        margin-bottom:20px;
+    }
+    .controller{
+        display:flex;
+        align-items: center;
+        height:48px;
+
+        .btn{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            padding:0px 16px;
+            height:36px;
+            border:1px solid #222222;
+            border-radius:20px;
+            margin-right:8px;
+            cursor:pointer;
+        }
+    }
+
+    .headerNotice{
+        height:69px;
+        display:flex;
+        align-items: center;
+        border-bottom:1px solid #dddddd;
+        color:#717171;
+
+    }
+
+    .coronaNotice{
+        height:42px;
+        display:flex;
+        align-items: flex-end;
+        
+        span{
+            text-decoration:underline;
+            margin-left:3px;
+            cursor:pointer;
+        }
+    }
+
+
+`;
+
