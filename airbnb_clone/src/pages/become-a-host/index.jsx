ed9a31a6e9e3e4a7368a2099635e49dff8cd0c4b
kimsanglok <evertree6031@gmail.com>
2021-08-08 04:50:47 +0900

    Feature : 호스팅 시작하기 1단계 ui 구성 및 css 완료 resolved :#20

--- airbnb_clone/src/pages/become-a-host/index.jsx
+++ airbnb_clone/src/pages/become-a-host/index.jsx
@@ -1,33 +1,101 @@
-import React, { useState } from 'react';
-import { BecomeHostBox } from './styled';
+import React, { useEffect, useState } from 'react';
+import { BecomeHostBox, HostIntroBox, HostStageBox } from './styled';
 import { Link, withRouter } from 'react-router-dom';
 
 const BecomeHostPage = () => {
     const [start, setStart] = useState(false);
+    const [stage1, setStage1] = useState(null);
+    const [curStage, setCurStage] = useState(null);
+
+    // useEffect(() => {
+    //     setStart(true);
+    //     // 임시로 방편
+    // }, []);
+
+
     return (
         <BecomeHostBox>
             {
                 !start &&
-                <>
-                    <div className="hostImg">
+                <HostIntroBox>
+                    <div className="host_intro_img">
                         <img src="/images/become_host/img/main.jpg" alt="" />
                         <Link to="/" className="logo">
                             <img src="/images/icons/red_logo.png" alt="" />
                         </Link>
                     </div>
-                    <div className="hostBody">
+                    <div className="host_intro_body">
                         <div className="tit">간단한 10단계로 호스팅<br />시작하기</div>
                         <div className="content">에어비앤비 호스트가 되어보세요. 에어비앤비에서 모든<br /> 과정을 도와드립니다.</div>
-                        <div className="bar"></div>
-                        <div className="startBtn" onClick={() => setStart(true)}>시작하기</div>
+                        <div className="btn">
+                            <div className="startBtn" onClick={() => { setStart(true); setCurStage(1) }}>시작하기</div>
+                        </div>
                         <Link to="/host/homes" className="exitBtn">나가기</Link>
                     </div>
-                </>
+                </HostIntroBox>
             }
             {
                 start &&
-                <>
-                </>
+                <HostStageBox>
+                    <div className="host_stage_img">
+                        <div className="stage_tit">
+                            호스팅할 숙소 유형을<br /> 알려주세요.
+                        </div>
+                        <Link to="/" className="logo">
+                            <img src="/images/favicon/favicon.png" alt="" />
+                        </Link>
+                    </div>
+
+
+                    <div className="host_stage_body">
+                        {
+                            curStage === 1 && (
+                                <>
+                                    <div className="opt_list">
+                                        <div className={stage1 === 0 ? "opt stage1_on" : "opt"} onClick={() => setStage1(0)}>
+                                            <div className="opt_tit">아파트</div>
+                                            <img src="/images/become_host/stage1/room1.png" alt="" />
+                                        </div>
+                                        <div className={stage1 === 1 ? "opt stage1_on" : "opt"} onClick={() => setStage1(1)}>
+                                            <div className="opt_tit">주택</div>
+                                            <img src="/images/become_host/stage1/room2.png" alt="" />
+                                        </div>
+                                        <div className={stage1 === 2 ? "opt stage1_on" : "opt"} onClick={() => setStage1(2)}>
+                                            <div className="opt_tit">별채</div>
+                                            <img src="/images/become_host/stage1/room3.png" alt="" />
+                                        </div>
+                                        <div className={stage1 === 3 ? "opt stage1_on" : "opt"} onClick={() => setStage1(3)}>
+                                            <div className="opt_tit">독특한 숙소</div>
+                                            <img src="/images/become_host/stage1/room4.png" alt="" />
+                                        </div>
+                                        <div className={stage1 === 4 ? "opt stage1_on" : "opt"} onClick={() => setStage1(4)}>
+                                            <div className="opt_tit">B&B</div>
+                                            <img src="/images/become_host/stage1/room5.png" alt="" />
+                                        </div>
+                                        <div className={stage1 === 5 ? "opt stage1_on" : "opt"} onClick={() => setStage1(5)}>
+                                            <div className="opt_tit">부티크 호텔</div>
+                                            <img src="/images/become_host/stage1/room6.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="btn">
+                                        <div className="beforeBtn" onClick={() => setStart(false)}>뒤로</div>
+                                        <div className="nextBtn" onClick={() => setCurStage(2)}>다음</div>
+                                    </div>
+                                </>
+                            )
+                        }
+                        {
+                            curStage === 2 &&
+                            (
+                                <>
+
+                                </>
+                            )
+
+                        }
+                        <Link to="/host/homes" className="exitBtn">나가기</Link>
+                    </div>
+                </HostStageBox>
             }
 
         </BecomeHostBox >
--- airbnb_clone/src/pages/become-a-host/styled.js
+++ airbnb_clone/src/pages/become-a-host/styled.js
@@ -3,25 +3,26 @@ import styled from "styled-components";
 export const BecomeHostBox = styled.div`
     width:100%;
     height: 100vh;
+`;
 
+export const HostIntroBox = styled.div` 
     display:flex;
+    width:100%;
+    height:100%;
 
-    .hostImg{
+    .host_intro_img{
         width:50%;
         height:100%;
         position:relative;
-
         img{
             width:100%;
             height:100%;
             object-fit:cover;
         }
-
         .logo{
             position:absolute;
             top:4%;
             left:6%;
-
             width:64px;
             height:64px;
             display:flex;
@@ -29,11 +30,9 @@ export const BecomeHostBox = styled.div`
             justify-content: center;
             border-radius:50%;
             cursor:pointer;
-
             &:hover{
                 background:rgba(128,128,128,0.25);
             }
-
             img{
                 width:32px;
                 height:32px;
@@ -41,7 +40,7 @@ export const BecomeHostBox = styled.div`
         }
     }
 
-    .hostBody{
+    .host_intro_body{
         width:50%;
         height:100%;
         display:flex;
@@ -49,12 +48,10 @@ export const BecomeHostBox = styled.div`
         justify-content: center;
         align-items:center;
         background:black;
-
         text-align:center;
         color:white;
-
-
         position:relative;
+
         .tit{
             font-weight:700;
             font-size:48px;
@@ -66,34 +63,33 @@ export const BecomeHostBox = styled.div`
             line-height:1.3;
         }
 
-        .bar{
+        .btn{
             position:absolute;
-            right:0;
-            bottom:11%;
+            bottom:0;
+            display:flex;
+            justify-content: flex-end;
+            align-items:center;
+            border-top:1px solid #717171;
+
             width:100%;
-            border-bottom:1px solid #717171;
-        }
+            padding:0 20px;
+            height:12%;
 
+            .startBtn{
+                z-index:100;
+                background:#dd1a60;
+                padding:16px 23px;
+                font-size:16px;
+                border-radius:10px;
+                color:white;
+                cursor:pointer;
+            }
+        }
         
-        .startBtn{
-            position:absolute;
-            right:6%;
-            bottom:3%;
-
-            z-index:100;
-            background:#dd1a60;
-            padding:16px 23px;
-            font-size:16px;
-            border-radius:10px;
-            color:white;
-            cursor:pointer;
-        }
-
         .exitBtn{
             position:absolute;
             right:6%;
             top:4%;
-
             z-index:100;
             background:#222222;
             padding:11px 20px;
@@ -103,6 +99,150 @@ export const BecomeHostBox = styled.div`
             font-weight:700;
             cursor:pointer;
         }
-        
     }
 `;
+
+
+
+export const HostStageBox = styled.div`
+    display:flex;
+    width:100%;
+    height:100%;
+
+    .host_stage_img{
+        width:50%;
+        height:100%;
+        position:relative;
+        display:flex;
+        align-items:center;
+
+        background: url('/images/become_host/img/background.png') no-repeat;
+        background-size:100% 100%;
+
+        .logo{
+            position:absolute;
+            top:4%;
+            left:6%;
+            width:48px;
+            height:48px;
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            border-radius:50%;
+            cursor:pointer;
+            background:rgba(255,255,255,0.9);
+            background:#ff5a5e;
+
+            img{
+                width:32px;
+                height:32px;
+            }
+        }
+
+        .stage_tit{
+            font-size:48px;
+            color:white;
+            font-weight:600;
+            padding-left:60px;
+            line-height:1.2;
+            letter-spacing :2px;
+        }
+    }
+
+    .host_stage_body{
+        width:50%;
+        height:100%;
+        display:flex;
+        flex-direction:column;
+        justify-content: center;
+        align-items:center;
+        text-align:center;
+        position:relative;
+
+        .opt_list{
+            width:70%;
+        
+            /* height:80%; */
+            height:70%;
+            overflow-y:scroll;
+
+            .opt{
+                display:flex;
+                border:1px solid gray;
+                align-items:center ;
+                justify-content:space-between;
+                padding:14px 25px;
+                width:100%;
+                border-radius:10px;
+                border:1px solid #dddddd;
+                margin-bottom:15px;
+
+                &:hover{
+                    border:1px solid black;
+                }
+
+                .opt_tit{
+                    font-size:18px;
+                    font-weight:700;
+                }
+                img{
+                    width:56px;
+                    height:56px;
+                }
+            }
+            .stage1_on{
+                background:#dddddd;
+                border:1px solid black;
+            }
+        }
+
+        .btn{
+            position:absolute;
+            bottom:0;
+            display:flex;
+            justify-content: space-between;
+            align-items:center;
+            border-top:1px solid #717171;
+
+            width:100%;
+            padding:0 45px;
+            height:12%;
+
+            .beforeBtn{
+                padding:16px 23px;
+                font-size:16px;
+                font-weight:700;
+                text-decoration:underline;
+                cursor:pointer;
+            }
+
+            .nextBtn{
+                z-index:100;
+                background:black;
+                padding:16px 23px;
+                font-size:16px;
+                border-radius:10px;
+                color:white;
+                cursor:pointer;
+            }
+        }
+        
+
+
+        .exitBtn{
+            position:absolute;
+            right:6%;
+            top:4%;
+            z-index:100;
+            background:#dddddd;
+            padding:11px 20px;
+            font-size:12px;
+            border-radius:40px;
+            font-weight:700;
+            cursor:pointer;
+        }
+
+
+    }
+
+`
