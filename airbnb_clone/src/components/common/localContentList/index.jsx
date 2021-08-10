657d7d11b08f45e9c857a819af71043786227c8d
kimsanglok <evertree6031@gmail.com>
2021-08-11 01:27:28 +0900

    Chores : test commit resolved :#30

--- airbnb_clone/src/components/common/localContentList/index.jsx
+++ airbnb_clone/src/components/common/localContentList/index.jsx
@@ -5,6 +5,8 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import StarRateIcon from '@material-ui/icons/StarRate';
 import { Link } from 'react-router-dom';
 
+
+//test
 const LocalContentList = ({ areas, rooms }) => {
     console.log(rooms);
     //한글, 영어 문자열의 바이트 계산하는 함수
