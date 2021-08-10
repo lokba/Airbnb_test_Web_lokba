db8c8d49d1c10567516b71055178adcc89209cec
kimsanglok <evertree6031@gmail.com>
2021-08-11 00:58:16 +0900

    Feature : api로 받아온 response 값으로 숙소 리스트 ui에 값 집어넣기 resolved :#29

--- airbnb_clone/src/pages/travel/index.jsx
+++ airbnb_clone/src/pages/travel/index.jsx
@@ -28,7 +28,7 @@ const TravelSearchPage = ({ location }) => {
         const loadAllRooms = async () => {
             const response = await axios.get('https://dev.rodin.club/rooms');
 
-            console.log(response.data);
+            setRooms(response.data.result);
         };
         loadAllRooms();
     }, []);
@@ -253,7 +253,7 @@ const TravelSearchPage = ({ location }) => {
                     {/* <div className="headerNotice">여행 날짜와 게스트 인원수를 입력하면 1박당 총 요금을 확인할 수 있습니다.</div> */}
                     <div className="coronaNotice">예약하기 전에 코로나19 관련 여행 제한 사항을 확인하세요. <span>자세히 알아보기</span></div>
                 </TravelContentHeader>
-                <LocalContentList areas={areas} />
+                <LocalContentList areas={areas} rooms={rooms} />
                 <Pagination />
             </TravelContent>
             <Footer />
