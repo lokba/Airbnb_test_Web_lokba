0640850acc2231d34fa1659eb78a531b8d06bf82
kimsanglok <evertree6031@gmail.com>
2021-08-11 07:49:18 +0900

    Feature : 프로필 소개 섹션 및 프로필 수정하기 섹션 ui 구성 및 css 완료 resolved :#33

--- airbnb_clone/src/pages/profile/index.jsx
+++ airbnb_clone/src/pages/profile/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/common/Header';
-import { ProfileBox, ProfileInfoBox, ProfilePageBlock } from './styled';
+import { CommentBox, MyCommentBox, ProfileBox, ProfileInfoBox, ProfilePageBlock } from './styled';
 import VerifiedUserOutlinedIcon from '@material-ui/icons/VerifiedUserOutlined';
+import HomeIcon from '@material-ui/icons/Home';
+import LocalMallIcon from '@material-ui/icons/LocalMall';
+import StarIcon from '@material-ui/icons/Star';
 
 const ProfilePage = () => {
+    const [openModifyBox, setOpenModifyBox] = useState(false);
+
     return (
         <ProfilePageBlock>
             <Header profile />
@@ -21,6 +26,62 @@ const ProfilePage = () => {
                     </div>
                 </ProfileBox>
                 <ProfileInfoBox>
+                    <header>
+                        <div className="title">안녕하세요. 저는 상록입니다.</div>
+                        <div className="register">회원 가입 : 2021</div>
+                        <u className={openModifyBox && "open"} onClick={() => setOpenModifyBox(true)}>프로필 수정하기</u>
+                    </header>
+                    <body>
+                        {
+                            openModifyBox ?
+                                (
+                                    <>
+                                        <div className="open_intro">
+                                            <div className="open_tit">소개</div>
+                                            <textarea name="open_introduction" />
+                                        </div>
+                                        <div className="open_location">
+                                            <div className="open_tit">위치</div>
+                                            <input name="location" className="inputBox" />
+                                        </div>
+                                        <div className="open_company">
+                                            <div className="open_tit">직장</div>
+                                            <input name="location" className="inputBox" />
+                                        </div>
+                                        <div className="ctrl">
+                                            <button className="cancelBtn" onClick={() => setOpenModifyBox(false)}>취소</button>
+                                            <button className="storeBtn">저장</button>
+                                        </div>
+                                    </>
+                                )
+                                :
+                                (
+                                    <>
+                                        <div className="intro">
+                                            <div className="intro_tit">소개</div>
+                                            <div className="intro_txt">fqewfqewgwgweg</div>
+                                        </div>
+                                        <div className="info">
+                                            <HomeIcon />
+                                            <div>거주지 : 서울 ,한국</div>
+                                        </div>
+                                        <div className="info">
+                                            <LocalMallIcon />
+                                            <div>직장 : ㄹㅂㄷㄹㅂㄷㄹㄹㅂㄷㅈㄹ</div>
+                                        </div>
+                                    </>
+                                )
+                        }
+                    </body>
+                    <CommentBox>
+                        <div className="tit">
+                            <StarIcon />
+                            <div>후기 0개</div>
+                        </div>
+                    </CommentBox>
+                    <MyCommentBox>
+                        <div className="tit">내가 작성한 후기</div>
+                    </MyCommentBox>
 
                 </ProfileInfoBox>
 
--- airbnb_clone/src/pages/profile/styled.js
+++ airbnb_clone/src/pages/profile/styled.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const ProfilePageBlock = styled.div`
     width:100%;
     height:100%;
-    border:1px solid red;
+
     .profile_body{
         margin:130px 15% 0 ;
+        display:flex;
+        justify-content: space-between;
     }
 `;
 
@@ -66,4 +68,147 @@ export const ProfileBox = styled.div`
     
 `;
 
-export const ProfileInfoBox = styled.div``;
+export const ProfileInfoBox = styled.div`
+    width:60%;
+    font-size:14px;
+    header{
+        margin-bottom:30px;
+        .title{
+            font-size:32px;
+            font-weight:600;
+            margin-bottom:10px;
+        }
+        .register{
+            color:#717171;
+            padding-bottom:20px;
+        }
+        u{
+            /* padding-top:16px; */
+            font-weight:500;
+            cursor:pointer;
+        }
+
+        .open{
+            cursor:not-allowed;
+            color:#dddddd;
+        }
+    }
+    body{
+        font-size:16px;
+        padding-bottom:35px;
+        border-bottom:1px solid #dddddd;
+        .intro{
+            .intro_tit{
+                font-size:24px;
+                font-weight:600;
+                padding-bottom:30px;
+            }
+            .intro_txt{
+                padding-bottom:30px;
+            }
+        }
+        .info{
+            display:flex;
+            align-items: center;
+            padding-bottom:16px;
+            svg{
+                width:20px;
+                height:20px;
+                margin-right:3px;
+            }
+            
+            div{
+                padding-top:3px;
+            }
+        }
+
+        .open_intro{
+            width:100%;
+            margin-bottom:30px;
+            textarea{
+                width:100%;
+                height:96px;
+                padding:8px;
+                font-size:16px;
+                border-radius:10px;
+                outline:none;
+                border:1px solid #717171;
+                box-sizing:border-box;
+            }
+        }
+        .open_location{
+            margin-bottom:30px;
+        }
+        .open_company{
+            margin-bottom:40px;
+        }
+
+        .open_tit{
+            padding-bottom:8px;
+        }
+        .inputBox{
+            width:100%;
+            padding:12px 12px 10px;
+            font-size:16px;
+            border-radius:10px;
+            border:1px solid #717171;
+            outline:none;
+            box-sizing:border-box;
+        }
+        .ctrl{
+            width:100%;
+            display:flex;
+            justify-content: space-between;
+
+            .cancelBtn{
+                padding:10px;
+                font-weight:600;
+                text-decoration:underline;
+                cursor:pointer;
+                background:none;
+                border:none;
+                font-size:16px;
+            }
+            .storeBtn{
+                padding:14px 24px;
+                background:#dddddd;
+                border:none;
+                font-size:16px;
+                cursor:pointer;
+                border-radius:10px;
+                color:white;
+            }
+        }
+    }
+`;
+
+export const CommentBox = styled.div`
+    padding:35px 0;
+    border-bottom:1px solid #dddddd;
+
+    .tit{
+        display:flex;
+        align-items:center;
+        
+        svg{
+            margin-right:5px;
+        }
+
+        div{
+            font-size:22px;
+            font-weight:600;
+            padding-top:5px;
+        }
+    }
+`;
+
+export const MyCommentBox = styled.div`
+    padding:35px 0;
+    border-bottom:1px solid #dddddd;
+
+    .tit{
+        text-decoration: underline;
+        font-weight:500;
+        cursor:pointer;
+    }
+`;
