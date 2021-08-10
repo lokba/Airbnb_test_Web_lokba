eef82fefaebb71fde361cec0d5b0ba8b38b9f8f7
kimsanglok <evertree6031@gmail.com>
2021-08-10 17:54:15 +0900

    Feature : 회원가입 정보(이메일, 이름, 생년월일, 암호) 상태값으로 담기 resolved :#25

--- airbnb_clone/src/components/common/InputBox/index.jsx
+++ airbnb_clone/src/components/common/InputBox/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { InputBox } from './styled';
 
-const InputModel = ({ birth_date, txt, name }) => {
+const InputModel = ({ birth_date, txt, name, onChange, signForm }) => {
     return (
         <InputBox>
             <div className="inputTit">{txt}</div>
             {
                 birth_date ?
-                    (<input type="date" />)
+                    (<input type="date" name={name} onChange={onChange} />)
                     :
-                    (<input className={name} placeholder={txt} />)
+                    (<input name={name} value={signForm[name]} className={name} placeholder={txt} onChange={onChange} />)
             }
         </InputBox>
     );
