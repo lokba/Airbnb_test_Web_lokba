422388cf9fb235d7cc185fc6930b6578ab501e61
kimsanglok <evertree6031@gmail.com>
2021-08-13 09:44:04 +0900

    Feature : 재사용성을 위한 구글맵 컴포넌트 수정 resolved :#45

--- airbnb_clone/src/components/common/map/index.jsx
+++ airbnb_clone/src/components/common/map/index.jsx
@@ -1,43 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-
-
-// class SimpleMap extends Component {
-//     static defaultProps = {
-//         center: {
-//             lat: 59.95,
-//             lng: 30.33
-//         },
-//         zoom: 11
-//     };
-
-//     render() {
-//         return (
-//             // Important! Always set the container height explicitly
-//             <div style={{ height: '100vh', width: '100%' }}>
-//                 <GoogleMapReact
-//                     bootstrapURLKeys={{ key: "AIzaSyBS03vrPecVdQPL69tU-wl_4nedBvRYcDk" }}
-//                     defaultCenter={this.props.center}
-//                     defaultZoom={this.props.zoom}
-//                 >
-//                     <AnyReactComponent
-//                         lat={59.955413}
-//                         lng={30.337844}
-//                         text="My Marker"
-//                     />
-//                 </GoogleMapReact>
-//             </div>
-//         );
-//     }
-// }
-
-const AnyReactComponent = ({ text }) => <div style={{ color: "red", fontSize: "20px" }}>{text}</div>;
+const AnyReactComponent = ({ text }) => <div style={{ color: "red", fontSize: "20px", }}>{text}</div>;
 
 
 export const SimpleMap = ({ detail }) => {
-
-    const center = { lat: Number(detail.roomLati), lng: Number(detail.roomLongi) };
+    const center = { lat: Number(detail[0].roomLati), lng: Number(detail[0].roomLongi) };
     return (
         <div style={{ height: '500px', width: '100%' }}>
             <GoogleMapReact
@@ -45,11 +13,16 @@ export const SimpleMap = ({ detail }) => {
                 defaultCenter={center}
                 defaultZoom={14}
             >
-                <AnyReactComponent
-                    lat={center.lat}
-                    lng={center.lng}
-                    text="marker"
-                />
+                {
+                    detail.map(v => (
+                        <AnyReactComponent
+                            lat={v.roomLati}
+                            lng={v.roomLongi}
+                            text="marker"
+                        />
+                    ))
+                }
+
             </GoogleMapReact>
         </div>
     );
