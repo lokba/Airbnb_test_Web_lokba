69cfddd9a20183de0ec610ea0bd17ec36cb5a362
kimsanglok <evertree6031@gmail.com>
2021-08-13 10:04:20 +0900

    Feature : 숙소 리스트 페이지에 구글 맵 연동 완료 resolved :#45

--- airbnb_clone/src/components/common/map/index.jsx
+++ airbnb_clone/src/components/common/map/index.jsx
@@ -4,27 +4,54 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div style={{ color: "red", fontSize: "20px", }}>{text}</div>;
 
 
-export const SimpleMap = ({ detail }) => {
+export const SimpleMap = ({ detail, list }) => {
     const center = { lat: Number(detail[0].roomLati), lng: Number(detail[0].roomLongi) };
     return (
-        <div style={{ height: '500px', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyBS03vrPecVdQPL69tU-wl_4nedBvRYcDk" }}
-                defaultCenter={center}
-                defaultZoom={14}
-            >
-                {
-                    detail.map(v => (
-                        <AnyReactComponent
-                            lat={v.roomLati}
-                            lng={v.roomLongi}
-                            text="marker"
-                        />
-                    ))
-                }
+        <>
+            {
+                list ?
+                    (
+                        <div style={{ height: '100%', width: '100%', position: "fixed" }} >
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: "AIzaSyBS03vrPecVdQPL69tU-wl_4nedBvRYcDk" }}
+                                defaultCenter={center}
+                                defaultZoom={7}
+                            >
+                                {
+                                    detail.map(v => (
+                                        <AnyReactComponent
+                                            lat={v.roomLati}
+                                            lng={v.roomLongi}
+                                            text="marker"
+                                        />
+                                    ))
+                                }
 
-            </GoogleMapReact>
-        </div>
+                            </GoogleMapReact>
+                        </div >
+                    )
+                    :
+                    (
+                        <div style={{ height: '500px', width: '100%' }} >
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: "AIzaSyBS03vrPecVdQPL69tU-wl_4nedBvRYcDk" }}
+                                defaultCenter={center}
+                                defaultZoom={14}
+                            >
+                                {
+                                    detail.map(v => (
+                                        <AnyReactComponent
+                                            lat={v.roomLati}
+                                            lng={v.roomLongi}
+                                            text="marker"
+                                        />
+                                    ))
+                                }
+
+                            </GoogleMapReact>
+                        </div >)
+            }
+        </>
     );
 }
 export default SimpleMap;
